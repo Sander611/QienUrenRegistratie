dd451c8d215c08746ae6e5aab6af49c00aa02035
Sander611 <sander_611@hotmail.com>
2019-11-15 16:10:54 +0100

    Account model gemaakt en database geinitieerd

---
+++ QienHoursRegistration/DataContext/RepositoryContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using QienHoursRegistration.Models;
+
+namespace QienHoursRegistration.DataContext
+{
+    public class RepositoryContext : DbContext
+    {
+        public RepositoryContext(DbContextOptions options)
+            :base(options)
+        {
+
+        }
+
+        public DbSet<Account> Accounts { get; set; }
+    }
+}
---
+++ QienHoursRegistration/Models/Account.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace QienHoursRegistration.Models
+{
+    [Table("Account")]
+    public class Account
+    {
+        [Key]
+        public int AccountId { get; set; }
+        public string Name { get; set; }
+
+        [Required]
+        public string Email { get; set; }
+        public DateTime? DateOfBirth { get; set; }
+
+        [Required]
+        public string HashedPassword { get; set; }
+        public string Address { get; set; }
+        public string ZIP { get; set; }
+        public string MobilePhone { get; set; }
+        public string City { get; set; }
+        public string IBAN { get; set; }
+        public DateTime? CreationDate { get; set; }
+        public string ProfileImage { get; set; }
+
+        [Required]
+        public bool IsAdmin { get; set; }
+        [Required]
+        public bool IsTrainee { get; set; }
+        [Required]
+        public bool IsQienEmployee { get; set; }
+        [Required]
+        public bool IsSeniorDeveloper { get; set; }
+        [Required]
+        public bool IsActive { get; set; }
+    }
+}
