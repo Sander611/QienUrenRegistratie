b4a9ea47e0f186fa86e4a0dc547562001a8a20fc
LoesPK <loeskockelmans@gmail.com>
2019-11-18 15:50:30 +0100

    added shared folder

---
+++ Shared/Models/Client.cs
@@ -0,0 +1,30 @@
+﻿using DataAnnotationsExtensions;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace Shared.Models
+{
+    public class Client
+    {
+        [Required]
+        public int ClientId { get; set; }
+        public int AccountId { get; set; }
+        [Required]
+        public string CompanyName { get; set; }
+        [Required]
+        public string ClientName1 { get; set; }
+        public string ClientName2 { get; set; }
+        [Required]
+        [Email]
+        [Remote(action: "VerifyEmail", controller: "ClientController")]
+        public string ClientEmail1 { get; set; }
+        [Email]
+        [Remote(action: "VerifyEmail", controller: "ClientController")]
+        public string ClientEmail2 { get; set; }
+    }
+}
