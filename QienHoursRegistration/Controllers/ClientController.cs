fe82e794522b435a945d7975fe35879854274687
Iana Detusheva <detusheva.iana@gmail.com>
2019-11-18 13:18:13 +0100

    interface, controller, repo for Clients

---
+++ QienHoursRegistration/Controllers/ClientController.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using QienHoursRegistration.Models;
+using QienHoursRegistration.Repositories;
+
+namespace QienHoursRegistration.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ClientController : ControllerBase
+    {
+        private readonly IClientRepository clientRepo;
+
+        public ClientController(IClientRepository clientRepo)
+        {
+            this.clientRepo = clientRepo;
+        }
+        [HttpGet]
+        public async Task<IEnumerable<Client>> GetAll()
+        {
+            return await clientRepo.Get();
+        }
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Client>> GetClientById(int id)
+        {
+            var client = await clientRepo.GetById(id);
+            if(client == null)
+            {
+                return NotFound();
+            }
+            return client;
+        }
+        [HttpPost]
+        public async Task<ActionResult<Client>> Create(Client client)
+        {
+            var existingClient =  await clientRepo.GetById(client.ClientId);
+            if (existingClient != null)
+            {
+                return Conflict("Kan een nieuwe klant niet tovoegen. De klant bestaat al in de DB");
+            }
+            clientRepo.Post(client);
+            return CreatedAtAction(nameof(GetClientById), new { id = client.ClientId }, client);
+        }
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var existingClient = await clientRepo.GetById(id);
+            if (existingClient == null)
+            {
+                return NotFound();
+            }
+            clientRepo.Delete(id);
+            return NoContent();
+        }
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Update(int id, Client client)
+        {
+            var existingClient = await clientRepo.GetById(id);
+            if (existingClient == null)
+            {
+                return BadRequest();
+            }
+            clientRepo.Update(client);
+            return NoContent();
+        }
+    }
+}
---
+++ QienHoursRegistration/Repositories/Interfaces/IClientRepository.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using QienHoursRegistration.Models;
+
+namespace QienHoursRegistration.Repositories
+{
+    public interface IClientRepository
+    {
+        Task<List<Client>> Get();
+        Task<Client> GetById(int id);
+        void Post(Client clientModel);
+        void Delete(int id);
+        void Update(Client client);
+    }
+}
