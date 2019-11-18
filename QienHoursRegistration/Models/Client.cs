f4d504df8ecb319f13f005d618b636ded22fe942
Iana Detusheva <detusheva.iana@gmail.com>
2019-11-18 09:44:19 +0100

    Added properties for clients

--- QienHoursRegistration/Models/Client.cs
+++ QienHoursRegistration/Models/Client.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,5 +9,14 @@ namespace QienHoursRegistration.Models
 {
     public class Client
     {
+        [Key]
+        public int ClientId { get; set; }
+        [ForeignKey("Account")]
+        public int AccountId { get; set; }
+        public string CompanyName { get; set; }
+        public string ClientName1 { get; set; }
+        public string ClientName2 { get; set; }
+        public string ClientEmail1 { get; set; }
+        public string ClientEmail2 { get; set; }
     }
 }
