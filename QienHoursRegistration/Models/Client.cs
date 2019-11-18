819c24ed10d92ea54ed9d3bd654b3744954000cd
Sander611 <sander_611@hotmail.com>
2019-11-18 09:52:45 +0100

    Client model dataannotts added

--- QienHoursRegistration/Models/Client.cs
+++ QienHoursRegistration/Models/Client.cs
@@ -7,15 +7,19 @@ using System.Threading.Tasks;
 
 namespace QienHoursRegistration.Models
 {
+    [Table("Client")]
     public class Client
     {
         [Key]
         public int ClientId { get; set; }
         [ForeignKey("Account")]
         public int AccountId { get; set; }
+        [Required]
         public string CompanyName { get; set; }
+        [Required]
         public string ClientName1 { get; set; }
         public string ClientName2 { get; set; }
+        [Required]
         public string ClientEmail1 { get; set; }
         public string ClientEmail2 { get; set; }
     }
