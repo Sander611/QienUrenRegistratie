c6dfb3661bec72f475654e5a65d50e104be73124
PengelPenguin <jasonpengel@gmail.com>
2019-11-21 13:39:59 +0100

    Employee dashboard view tables

--- UrenProjectQien/Controllers/EmployeeController.cs
+++ UrenProjectQien/Controllers/EmployeeController.cs
@@ -2,15 +2,32 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Shared.Models;
 using Microsoft.AspNetCore.Mvc;
+using UrenProjectQien.Helper;
+using System.Net.Http;
 
 namespace UrenProjectQien.Controllers
 {
     public class EmployeeController : Controller
     {
-        public IActionResult EmployeeDashboard()
+        ApiHelper _api = new ApiHelper();
+        
+        public async Task<IActionResult> EmployeeDashboard()
         {
-            return View();
+            //HttpClient httpClient = _api.initial(); 
+            AccountModel account = new AccountModel()
+            {
+                FirstName = "Romy",
+                LastName = "van der Sar",
+                City = "Utrecht",
+                Address = "Langelaan 45",
+                DateOfBirth = new DateTime(1995, 8, 6)
+                
+
+
+            };
+            return View(account);
         }
     }
 }
