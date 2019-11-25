0f835fb7453453683e7166db029b985da0d6a7a4
PengelPenguin <jasonpengel@gmail.com>
2019-11-25 13:06:47 +0100

    Account role

---
+++ Shared/Entities/AccountRole.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Shared.Entities
+{
+    public class AccountRole : IdentityRole<int>
+    {
+
+    }
+}
